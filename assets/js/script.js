b0711a23464cf662909bc8e2bbb5cab94f5c6357
Abdulbasit Adio <94764960+Ayam7273@users.noreply.github.com>
2023-12-13 07:59:35 +0100

    section scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,4 +108,43 @@ for (var i = 0; i < li.length; i++) {
   });
 }
 
-// Doctors & Team
+
+// Scrolling and active link highlighting 
+  document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    navLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+          window.scrollTo({
+            top: targetSection.offsetTop - document.querySelector('#navbar-menu').offsetHeight,
+            behavior: 'smooth'
+          });
+
+          // Remove active class from all links
+          navLinks.forEach(link => link.classList.remove('active'));
+
+          // Add active class to the clicked link
+          this.classList.add('active');
+        }
+      });
+    });
+
+    // Highlight the active link based on scroll position
+    window.addEventListener('scroll', function () {
+      const fromTop = window.scrollY + document.querySelector('#navbar-menu').offsetHeight;
+
+      navLinks.forEach(link => {
+        const section = document.getElementById(link.getAttribute('href').substring(1));
+
+        if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+          navLinks.forEach(link => link.classList.remove('active'));
+          link.classList.add('active');
+        }
+      });
+    });
+  });
